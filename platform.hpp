c7d10f30b3f5b5e3a706127e86e435f65adc0a91
Johan Sandred <johan.sandred@pm.me>
2019-05-22 17:26:47 +0200

    Temporarily use SDL for Windows by default
    Since there is no platform file for windows yet, default to SDL.

--- platform.hpp
+++ platform.hpp
@@ -4,8 +4,10 @@
 #include "sdlmain.hpp"
 using namespace platform::SDL;
 #elif defined(_WIN64) || defined(_WIN32)
-#include "winmain.hpp"
-using namespace platform::windows;
+/* #include "winmain.hpp" */
+/* using namespace platform::windows; */
+#include "sdlmain.hpp" // tmp
+using namespace platform::SDL;
 #elif defined(__linux__)
 #include "sdlmain.hpp"
 using namespace platform::SDL;
