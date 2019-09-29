d91a7e872899c64de62f372cb5a59fb0b09b85dc
Johan Sandred <johan.sandred@protonmail.com>
2019-09-29 18:18:13 +0200

    Provide access to all normalized draw functions

--- src/draw.hpp
+++ src/draw.hpp
@@ -23,7 +23,10 @@ struct Square {
 using Point = V2f;
 
 auto draw_solid_square(BackBuffer& buf, Square sqr, RGB color, uint a = 0xff) -> void;
-auto draw_text_normalized(BackBuffer& buf, std::string_view text, float x, float y, float pixelHeight) -> void;
+auto draw_solid_square_normalized(BackBuffer& buf, Square sqr, RGB color, uint a = 0xff) -> void;
 auto draw_hollow_square(BackBuffer& buf, Square sqr, RGB color, int a = 0xff, int borderSize = 1) -> void;
+auto draw_hollow_square_normalized(BackBuffer& buf, Square sqr, RGB color, int a = 0xff, int borderSize = 1) -> void;
 auto draw_font_string(BackBuffer& buf, FontString& fontString, int x, int y) -> void;
+auto draw_font_string_normalized(BackBuffer& buf, FontString& fontString, float x, float y) -> void;
 auto draw_text(BackBuffer& buf, std::string_view text, int x, int y, float pixelHeight) -> void;
+auto draw_text_normalized(BackBuffer& buf, std::string_view text, float x, float y, float pixelHeight) -> void;
