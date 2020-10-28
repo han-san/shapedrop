c5df0c0025ce66d822926b9ac604f8b7fec248a2
Johan Sandred <johan.sandred@protonmail.com>
2020-10-28 20:38:58 +0100

    Sleep to limit the FPS

--- src/core.cpp
+++ src/core.cpp
@@ -40,6 +40,10 @@ auto run() -> void
         auto const newFrameStartClock {std::chrono::high_resolution_clock::now()};
         programState.frameTime = newFrameStartClock - programState.frameStartClock;
         programState.frameStartClock = newFrameStartClock;
+        if (programState.frameTime < ProgramState::targetFrameTime) {
+            auto const sleepTime {ProgramState::targetFrameTime - programState.frameTime};
+            std::this_thread::sleep_for(sleepTime);
+        }
 
         // input
         handle_input(programState, gameState);
